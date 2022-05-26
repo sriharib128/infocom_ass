01312a65fea406f46e2e9463bce6c4d5575454fc
Srihari <sriharib128@gmail.com>
2022-05-26 12:46:25 +0530

    parse_print

--- Assignment1/getPostion.c
+++ Assignment1/getPostion.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include<stdlib.h>
 
+int maximum (int a, int b)
+{
+    if(a<=b)
+        return b;
+    return a;
+}
+
 int * MatchLengthPosition(char * window, char * text)
 {
     int *A = (int * )malloc(sizeof(int)*3);
@@ -23,6 +30,36 @@ int * MatchLengthPosition(char * window, char * text)
         A[1]=text[0];
     }
     return A;
+}
+
+void getsubstring(char * window,char * InputText , int start , int end)
+{
+    int j=0;
+    for(int i=start;i<end;i++)
+        window[j++]=InputText[i];
+    window[j]='\0';
+}
+
+void ParseSWLZ(char * InputText,int WindowSize)
+{
+    for(int i=0;i<strlen(InputText);i++)
+    {
+        char window[WindowSize+1];
+        window[0]='\0';
+        int start = maximum(0,i-16);
+        int end = i;
+        getsubstring(window,InputText,start,end);//gives substring from start to end -1
+        printf("%s-%d\n",window,strlen(window));
+        for(int j=0;j<strlen(window);j++)
+        {
+            char subwindow[WindowSize+1];
+            subwindow[0]='\0';
+            start = j;end = i;
+            getsubstring(subwindow,window,start,end);
+            printf("\t\t>%s-%d\n",subwindow,strlen(subwindow));
+        }
+        printf("----------------\n");
+    }
 }
 
 int main()
@@ -57,12 +94,12 @@ int main()
 
     // printf("%s%s\n",window,text);
     
-    int* A  = MatchLengthPosition(window,text);
-    if(A[0]==1)
-        printf("[%d,%d,%d]\n",A[0],A[1],A[2]);
-    else
-        printf("[%d,%c]\n",A[0],A[1]);
+    // int* A  = MatchLengthPosition(window,text);
+    // if(A[0]==1)
+    //     printf("[%d,%d,%d]\n",A[0],A[1],A[2]);
+    // else
+    //     printf("[%d,%c]\n",A[0],A[1]);
+    ParseSWLZ(text,16);
     printf("----------------------------------\n");
     }
-
 }
